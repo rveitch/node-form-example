7cfc4e1824aab7952e91f8f91a47d65be1cdd08b
ryan.veitch <ryan.r.veitch@gmail.com>
2016-12-02 03:19:18 -0600

    Added Sign-in Form to Token response example

--- README.md
+++ README.md
@@ -10,3 +10,22 @@
 - `http://localhost:3000/` - Default endpoint
 - `http://localhost:3000/hi` - Plain text response example
 - `http://localhost:3000/user?username=nick&password=awesomestuff&apikey=123456789` - Query parameter parsting example
+
+If your form looks like this:
+```html
+<form action="http://localhost:3000/submit" method="post">
+		<input type="text" placeholder="username" name="username">
+		<input type="password" placeholder="password" name="password">
+		<input type="submit">
+</form>
+```
+
+you can access the POST submitted text data this way:
+```javascript
+console.log('Username: ' + req.body.username);
+console.log('Password: ' + req.body.password);
+```
+
+#### Reference
+- https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
+- https://github.com/request/request#http-authentication
--- index.js
+++ index.js
@@ -3,21 +3,27 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var bodyParser = require('body-parser')
+var uuidV4 = require('uuid/v4');
 var port = Number(process.env.PORT || 3000);
 
-app.set('json spaces', 2); // Tell Express to pretty print json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('json spaces', 2); // Set Express to pretty print json
+app.set('view engine', 'ejs'); // Set Express's view engine to EJS
 
+// Declare the schema as a global variable/object to set values to later
+var responseMessage = {
+	username: null,
+	password: null,
+	token: null,
+};
+
+/******************** EXPRESS ENDPOINTS ********************/
+
+// Default Endpoint
 app.get('/', function (req, res) {
-	// Create a plain js object for the json
-	var genericMessage = {
-		username: null,
-		password: null,
-		apikey: null,
-	};
-
-	res.json(genericMessage); // Response: send the variable as json
+	res.render('index'); // Render an HTML page from an .ejs template
 });
-
 
 // EXAMPLE ENDPOINT: http://localhost:3000/hi
 app.get('/hi', function (req, res) {
@@ -25,14 +31,50 @@ app.get('/hi', function (req, res) {
 });
 
 // EXAMPLE ENDPOINT - Query Parameters:
-// http://localhost:3000/user?username=nick&password=awesomestuff&apikey=123456789
+// http://localhost:3000/user?username=nick&password=awesomestuff&token=123456789
 app.get('/user', function (req, res) {
 	var genericMessage = {
 		username: req.query['username'],
 		password: req.query['password'],
-		apikey: req.query['apikey'],
+		token: req.query['token'],
 	};
 	res.json(genericMessage);
+});
+
+// EXAMPLE ENDPOINT - API Key: Provide a fake external endpoint
+app.get('/token', function(req, res) {
+	var token = {
+		token: uuidV4() // generates a random UUID
+	};
+	res.json(token);
+});
+
+/* EXAMPLE ENDPOINT - POST requests */
+app.post('/submit', function(req, res) {
+	console.log(req.body); // Form POST request body
+
+	// Update the responseMessage values
+	responseMessage.username = req.body.username;
+	responseMessage.password = req.body.password;
+
+	// Set the external request options
+	var targetURL = req.headers.host; // get the current host address in case it isnt localhost
+	var options = {
+		method: 'GET',
+	  url: 'http://'+ targetURL + '/token',
+	  headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	// Send the external request
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+		var responseBody = JSON.parse(body); // parse the response body
+			console.log(responseBody);
+		responseMessage.token = responseBody.token; // Update the responseMessage values
+		res.json(responseMessage); // return the final resonse to the /submit endpoint
+	})
 });
 
 app.listen(port, function () {
